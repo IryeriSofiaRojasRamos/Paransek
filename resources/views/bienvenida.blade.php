9250aed80c39be2491d9007cc31ad2093793f32d
Iryeri <sof.rojram@gmail.com>
2021-04-16 15:02:40 -0600

    Instalación de bootstrap

--- resources/views/bienvenida.blade.php
+++ resources/views/bienvenida.blade.php
@@ -8,6 +8,11 @@
 		
 		<!--HOLAS DE ESTILO-->
 			<link href="{{ asset('./css/estilo_menu.css') }}?v=<?php echo(rand()); ?>" rel="stylesheet">
+			
+			<link rel="stylesheet" href="css/bootstrap.min.css">
+			<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+			<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+			<script src="js/bootstrap.min.js"></script>
 		
 		<title>PARANSEK</title>
 	</head>
@@ -17,7 +22,7 @@
 		<input type="checkbox" id="check">
 		
 		<label for="check">
-			<img src="./imagenes/menugif.gif">
+			<img src="./imagenes/menu.gif">
 		</label>
 		
 		<nav>
@@ -42,7 +47,7 @@
 				<li><a href="">Contactos</a></li>
 			</ul>
 		</nav>
-		
+
 	</body>
 
 </html>
