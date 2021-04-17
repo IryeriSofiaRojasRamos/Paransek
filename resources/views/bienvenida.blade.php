ea3e1681acdefc1e8c3064f27a5b9a9eb837d3b1
Iryeri <sof.rojram@gmail.com>
2021-04-16 21:57:32 -0600

    Cambios relacionados a página de bienvenida

--- resources/views/bienvenida.blade.php
+++ resources/views/bienvenida.blade.php
@@ -3,18 +3,7 @@
 <html>
 
 	<head>
-		<meta charset="utf-8">
-		<link rel="icon" type="image/jpg" href="./fondos/icono.jpg"/>
-		
-		<!--HOLAS DE ESTILO-->
-			<link href="{{ asset('./css/estilo_menu.css') }}?v=<?php echo(rand()); ?>" rel="stylesheet">
-			
-			<link rel="stylesheet" href="css/bootstrap.min.css">
-			<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-			<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-			<script src="js/bootstrap.min.js"></script>
-		
-		<title>PARANSEK</title>
+		<link href="{{ asset('./css/estilo_menu.css') }}?v=<?php echo(rand()); ?>" rel="stylesheet">
 	</head>
 
 	<body>
@@ -40,11 +29,11 @@
                 	@endauth
                 @endif
 
-				<li><a href="">Paransek</a></li>
-				<li><a href="">Atrapasueños</a></li>
-				<li><a href="">Bolsas de compras</a></li>
-				<li><a href="">Tarjeteros</a></li>
-				<li><a href="">Contactos</a></li>
+				<li><a href="">Objetivos</a></li>
+				<li><a href="">Misión</a></li>
+				<li><a href="">Visión</a></li>
+				<li><a href="">Productos</a></li>
+				<li><a href="">Redes Sociales</a></li>
 			</ul>
 		</nav>
 
